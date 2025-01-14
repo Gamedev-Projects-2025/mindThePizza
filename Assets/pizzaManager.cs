35cf312a0544d6df0c742311284f8f8a20f8c6ba
Lidor Borofker <155881749+Lidx7@users.noreply.github.com>
2025-01-14 13:46:00 +0200

    Update pizzaManager.cs

--- Assets/pizzaManager.cs
+++ Assets/pizzaManager.cs
@@ -4,15 +4,17 @@ public class pizzaManager : MonoBehaviour
 {
     public Pizza myPizza;
     [SerializeField] private string target;
+    private float ingredOffsetX;
+    private float ingredOffsetY;
+    [SerializeField] public int numOfIngred = 3;
+
 
     void Start()
     {
-        // Initialization if needed
     }
 
     void Update()
     {
-        // Update logic if needed
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -32,6 +34,30 @@ public class pizzaManager : MonoBehaviour
                 Destroy(cloneCollider); // or cloneCollider.enabled = false;
             }
 
+            switch (numOfIngred % 4)
+            {
+                case 0:
+                    ingredOffsetX = 2f;
+                    ingredOffsetY = 0;
+                    break;
+
+                case 1:
+                    ingredOffsetX = -2f;
+                    ingredOffsetY = 0;
+                    break;
+
+                case 2:
+                    ingredOffsetX = 0;
+                    ingredOffsetY = 2f;
+                    break;
+
+                case 3:
+                    ingredOffsetX = 0;
+                    ingredOffsetY = -2f;
+                    break;
+            }
+            numOfIngred--;
+
             // Copy the sprite renderer and set it to the center of the pizza
             SpriteRenderer originalSpriteRenderer = collision.gameObject.GetComponent<SpriteRenderer>();
             SpriteRenderer cloneSpriteRenderer = ingredientClone.GetComponent<SpriteRenderer>();
@@ -40,8 +66,13 @@ public class pizzaManager : MonoBehaviour
             {
                 cloneSpriteRenderer.sprite = originalSpriteRenderer.sprite; // Copy the sprite
 
+                if (cloneSpriteRenderer.sprite.name == "tomato")
+                {
+                    cloneSpriteRenderer = Resources.Load<Sprite>("tomato_sauce");
+                }
+
                 // Position the clone at the center of the pizza (you can adjust the position as needed)
-                ingredientClone.transform.position = new Vector3(gameObject.transform.position.x,gameObject.transform.position.y,-1f);
+                ingredientClone.transform.position = new Vector3(gameObject.transform.position.x + ingredOffsetX, gameObject.transform.position.y + ingredOffsetY, -1f);
 
                 // Scale up the clone (adjust the scale factor as needed)
                 ingredientClone.transform.localScale = ingredientClone.transform.localScale * 3f;
